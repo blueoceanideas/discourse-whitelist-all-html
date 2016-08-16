d71c7cc6988fcfabed44b2780fc879e2336bd303
Benjamin <rhodel@live.it>
2016-08-16 13:29:18 +0200

    Delete plugin.rb

--- plugin.rb
+++
@@ -1,9 +0,0 @@
-# name: UPPA Optinmonster
-# about:  Integra OptinMonster a Discourse
-# version: 0.0.1
-# authors: Benjamin Meghnagi
-
-enabled_site_setting :uppa_enabled
-register_asset "javascripts/uppa-manipola.js", :server_side
-
-
