a994eb2e53e52b3c5f00d0a3dcc84a1dbec84200
Benjamin <rhodel@live.it>
2016-08-16 13:37:28 +0200

    Correction

--- plugin.rb
+++ plugin.rb
@@ -6,4 +6,13 @@
 enabled_site_setting :uppa_enabled
 register_asset "javascripts/uppa-manipola.js", :server_side
 
+# Without this, all the Onebox stuff isn't loaded,
+# so methods like `matches_regexp` do not work.
+# It's weird. I know.
+Onebox = Onebox
+class Onebox::Engine::uppa
+  include Onebox::Engine
 
+  def self.priority
+    0
+  end
