5250cf2856a16091765c6126776758fb1ef40b8c
Benjamin <rhodel@live.it>
2016-08-16 18:53:20 +0200

    Update serious

--- assets/javascripts/uppa-manipola.js
+++ assets/javascripts/uppa-manipola.js
@@ -1,13 +1,3 @@
-function resolveDOI (text) { 
-  return text.replace("superuppadiv", "<div ");
-}
-
-Discourse.Dialect.postProcessText(function (text) {
-  text = [].concat(text);
-  for (var i = 0; i < text.length; i++) {
-    if (text[i].length > 0 && text[i][0] !== "<") {
-      text[i] = resolveDOI(text[i]);
-    }
-  }
-  return text;
-});
+(function () {
+  Discourse.Markdown.whiteListTag('div', 'id', 'om-xddddihwxnstqcdk-holder');
+})();
