f85b285ceb7529c146f54af3dd44ee8e999ec3bc
Benjamin <rhodel@live.it>
2016-08-16 13:56:16 +0200

    Update

--- assets/javascripts/uppa-manipola.js
+++ assets/javascripts/uppa-manipola.js
@@ -1,13 +1,12 @@
-function piratize (text) {
-  return text.replace(/\b(superuppaclose)\b/ig, "></div>")
-             .replace(/superuppadiv/ig, "<div ");
+function resolveDOI (text) { 
+  return text.replace("superuppadiv", "<div ");
 }
 
 Discourse.Dialect.postProcessText(function (text) {
   text = [].concat(text);
   for (var i = 0; i < text.length; i++) {
     if (text[i].length > 0 && text[i][0] !== "<") {
-      text[i] = piratize(text[i]);
+      text[i] = resolveDOI(text[i]);
     }
   }
   return text;
