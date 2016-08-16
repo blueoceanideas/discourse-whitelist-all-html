69ef44bc8ab950040883171b4e6681d3fe5429cf
Benjamin <rhodel@live.it>
2016-08-16 13:30:12 +0200

    Version with changing live of superuppaclose and superuppadiv

---
+++ plugin.rb
@@ -0,0 +1,9 @@
+# name: UPPA Optinmonster
+# about:  Integra OptinMonster a Discourse
+# version: 0.0.1
+# authors: Benjamin Meghnagi
+
+enabled_site_setting :uppa_enabled
+register_asset "javascripts/uppa-manipola.js", :server_side
+
+
