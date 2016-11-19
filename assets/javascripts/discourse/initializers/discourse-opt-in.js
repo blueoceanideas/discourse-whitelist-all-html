2b2e90243e92d6ab4bf0705a6764f63f6f7309af
Benjamin <rhodel@live.it>
2016-11-19 18:40:33 +0100

    Update and rename discourse-opt-in.js.es6 to discourse-opt-in.js

--- assets/javascripts/discourse/initializers/discourse-opt-in.js
+++ assets/javascripts/discourse/initializers/discourse-opt-in.js
@@ -1,10 +1,4 @@
-import { withPluginApi } from 'discourse/lib/plugin-api';
-
-function initializePlugin(api)
-{
-  api.onPageChange((url, title) => {
-        // your code here
-  var om570ceb4c51994, om570ceb4c51994_poll = function() {
+var om570ceb4c51994, om570ceb4c51994_poll = function() {
     var r = 0;
     return function(n, l) {
       clearInterval(r), r = setInterval(n, l)
@@ -48,15 +42,3 @@ function initializePlugin(api)
     };
     (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(o)
   }(document, "script", "omapi-script");
-    
-    console.log('the page changed to: ' + url + ' and title ' + title);
-  });
-}
-
-export default {
-  name: 'dynamic-table',
-  initialize: function(container)
-  {
-    withPluginApi('0.1', api => initializePlugin(api));
-  }
-};
