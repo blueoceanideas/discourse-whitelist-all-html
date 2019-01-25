18521ce4074a861d83479cf27341401ec8450350
Luke Abell <luke@abelltech.com>
2019-01-25 13:16:03 -0500

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,4 @@
-# name: UPPA Optinmonster
+# name: Whitelist All HTML Tags
 # about: Whitelist all HTML tags custom classes and ID's
-# version: 0.0.1
-# authors: Benjamin Meghnagi
-
+# version: 0.0.2
 enabled_site_setting :uppa_enabled
